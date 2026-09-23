e509ce5092fc0c6f254a184f9a61f1b2b86c37b1
David Umberto Alvaro <umb.alvaro@gmail.com>
2026-09-23 04:06:57 +0200

    Update reio_drive.h

--- soft/reio_drive.h
+++ soft/reio_drive.h
@@ -4,8 +4,6 @@
 #include <stdint.h>
 #include <stddef.h>
 
-// Point d'entrée du Pont Invariant de la bibliothèque binaire Rust unifiée
-// Inclut désormais la vérification de la validité temporelle de la licence (Time-bomb)
 uint32_t verifier_flux_reio(const uint8_t *buffer_ptr, 
                             size_t taille, 
                             uint32_t annee_actuelle, 
